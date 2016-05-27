f9d8f5e0578f88416af5af98e9bb46a05804e385
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2016-05-27 11:52:15 +0300

    tests: Split RPF->WPF test
    The VSP instance that supports the largest number of RPF might not be
    the same as the one that supports the largest number of WPF. To maximize
    test coverage, split the test into RPF and WPF tests that can use
    different VSP instances.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>

--- tests/vsp-unit-test-0005.sh
+++ tests/vsp-unit-test-0005.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
 #
-# Test RPF -> WPF with all RPF and WPF instances in sequence. The format
-# doesn't matter much, use RGB24 to simplify frame comparison.
+# Test RPF -> WPF with all RPF instances in sequence. The format doesn't matter
+# much, use RGB24 to simplify frame comparison.
 #
 
 source vsp-lib.sh
 
-features="rpf.0 rpf.1 wpf.0 wpf.1"
+features="rpf.0 rpf.1 wpf.0"
+optional_features="rpf.2 rpf.3 rpf.4"
 format=RGB24
 
 test_rpf() {
@@ -26,35 +27,13 @@ test_rpf() {
 	test_complete $result
 }
 
-test_wpf() {
-	wpf=$1
-
-	test_start "WPF.$wpf"
-
-	pipe_configure rpf-wpf 0 $wpf
-	format_configure rpf-wpf 0 $wpf $format 1024x768 $format
-
-	$vsp_runner $mdev input 0 $format &
-	$vsp_runner $mdev output $wpf $format
-
-	result=$(compare_frames exact reference $format $wpf)
-
-	test_complete $result
-}
-
 test_main() {
 	num_rpfs=$(vsp1_count_rpfs)
-	num_wpfs=$(vsp1_count_wpfs)
 
 	for rpf in `seq 0 1 $((num_rpfs-1))` ; do
 		test_rpf $rpf
 	done
-
-	# Skip WPF.0, it has already been tested during the RPF tests.
-	for wpf in `seq $((num_wpfs-1))` ; do
-		test_wpf $wpf
-	done
 }
 
-test_init $0 "$features"
+test_init $0 "$features" "$optional_features"
 test_run
---
+++ tests/vsp-unit-test-0009.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+#
+# Test RPF -> WPF with all WPF instances in sequence. The format doesn't matter
+# much, use RGB24 to simplify frame comparison.
+#
+
+source vsp-lib.sh
+
+features="rpf.0 wpf.0 wpf.1"
+optional_features="wpf.1 wpf.2 wpf.3"
+format=RGB24
+
+test_wpf() {
+	wpf=$1
+
+	test_start "WPF.$wpf"
+
+	pipe_configure rpf-wpf 0 $wpf
+	format_configure rpf-wpf 0 $wpf $format 1024x768 $format
+
+	$vsp_runner $mdev input 0 $format &
+	$vsp_runner $mdev output $wpf $format
+
+	result=$(compare_frames exact)
+
+	test_complete $result
+}
+
+test_main() {
+	num_wpfs=$(vsp1_count_wpfs)
+
+	for wpf in `seq 0 1 $((num_wpfs-1))` ; do
+		test_wpf $wpf
+	done
+}
+
+test_init $0 "$features" "$optional_features"
+test_run
