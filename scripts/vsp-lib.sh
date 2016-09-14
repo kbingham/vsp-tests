kbingham/vsp-tests

commit ccd867a8486326393f3a57d8b67c73db0bfc582b

vsp-lib: SRU and rotation are not perfectly emulated by gen-image
Turn on fuzzy comparison for rotation and for the SRU regardless of the
scaling ratio.

Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>

scripts/vsp-lib.sh
@@ -146,7 +146,10 @@ reference_frame() {
 			options="$options --lut $value"
 			;;
 		rotate)
-			[ x$value = x90 ] && options="$options --rotate"
+			[ x$value = x90 ] && {
+				options="$options --rotate" ;
+				__vsp_pixel_perfect=false ;
+			}
 			;;
 		vflip)
 			[ x$value = x1 ] && options="$options --vflip"
@@ -241,7 +244,7 @@ compare_frames() {
 	local size=$(vsp1_entity_get_size wpf.$wpf 1)
 	local method=exact
 
-	if [ x$__vsp_uds_scale = xtrue ] ; then
+	if [ $__vsp_pixel_perfect != xtrue ] ; then
 		method=fuzzy
 	fi
 
@@ -420,9 +423,9 @@ pipe_reset() {
 
 	__vsp_bru_inputs=
 	__vsp_rpf_format=
-	__vsp_uds_scale=
 	__vsp_wpf_index=
 	__vsp_wpf_format=
+	__vsp_pixel_perfect=true
 }
 
 pipe_configure() {
@@ -512,7 +515,7 @@ format_rpf_bru_uds() {
 	$mediactl -d $mdev -V "'$dev wpf.0':0 [fmt:$infmt/$outsize]"
 	$mediactl -d $mdev -V "'$dev wpf.0':1 [fmt:$outfmt/$outsize]"
 
-	[ $insize != $outsize ] && __vsp_uds_scale=true
+	[ $insize != $outsize ] && __vsp_pixel_perfect=false
 	__vsp_rpf_format=$1
 	__vsp_wpf_format=$3
 }
@@ -572,7 +575,7 @@ format_rpf_uds() {
 	$mediactl -d $mdev -V "'$dev wpf.0':0 [fmt:$infmt/$outsize]"
 	$mediactl -d $mdev -V "'$dev wpf.0':1 [fmt:$outfmt/$outsize]"
 
-	[ $insize != $outsize ] && __vsp_uds_scale=true
+	[ $insize != $outsize ] && __vsp_pixel_perfect=false
 	__vsp_rpf_format=$1
 	__vsp_wpf_format=$3
 }
@@ -593,7 +596,7 @@ format_rpf_uds_bru() {
 	$mediactl -d $mdev -V "'$dev wpf.0':0 [fmt:$infmt/$outsize]"
 	$mediactl -d $mdev -V "'$dev wpf.0':1 [fmt:$outfmt/$outsize]"
 
-	[ $insize != $outsize ] && __vsp_uds_scale=true
+	[ $insize != $outsize ] && __vsp_pixel_perfect=false
 	__vsp_rpf_format=$1
 	__vsp_wpf_format=$3
 }
@@ -610,7 +613,7 @@ format_rpf_sru() {
 	$mediactl -d $mdev -V "'$dev wpf.0':0 [fmt:$infmt/$outsize]"
 	$mediactl -d $mdev -V "'$dev wpf.0':1 [fmt:$outfmt/$outsize]"
 
-	[ $insize != $outsize ] && __vsp_uds_scale=true
+	__vsp_pixel_perfect=false
 	__vsp_rpf_format=$1
 	__vsp_wpf_format=$3
 }

commit 32c8be484cf81717471909c7fa4dd4635d3a01c8

vsp-lib: Fix fuzzy comparison
A typo caused fuzzy comparison to be used in all tests. Fix it, and make
sure the fuzzy comparison gets properly enabled when using rotation.

Fixes: ccd867a84863 ("vsp-lib: SRU and rotation are not perfectly emulated by gen-image")
Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>

scripts/vsp-lib.sh
@@ -242,19 +242,19 @@ compare_frames() {
 	local in_fmt=$(echo $in_format | tr '[:upper:]' '[:lower:]')
 	local out_fmt=$(echo $out_format | tr '[:upper:]' '[:lower:]')
 	local size=$(vsp1_entity_get_size wpf.$wpf 1)
-	local method=exact
-
-	if [ $__vsp_pixel_perfect != xtrue ] ; then
-		method=fuzzy
-	fi
 
 	reference_frame ${frames_dir}ref-frame.bin $in_format $out_format $size $args
 
+	local method=exact
 	local result="pass"
 	local params=${args// /-}
 	params=${params:+-$params}
 	params=${params//\//_}
 	params=$in_fmt-$out_fmt-$size$params
+
+	if [ x$__vsp_pixel_perfect != xtrue ] ; then
+		method=fuzzy
+	fi
 
 	for frame in ${frames_dir}frame-*.bin ; do
 		local match="true"