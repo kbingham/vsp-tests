50123fc029703b0e8aff01f09679dc18978f1275
Kieran Bingham <kieran.bingham@ideasonboard.com>
2017-02-10 13:30:08 +0000

    tests: Add RPF cropping test
    Test both the input cropping size and position
    
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>
    Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>

---
+++ tests/vsp-unit-test-0018.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+#
+# Test RPF crop using RGB. Use a RPF -> WPF pipeline, passing a selection of
+# cropping windows.
+#
+
+source vsp-lib.sh
+
+features="rpf.0 wpf.0"
+crops="(0,0)/512x384 (32,32)/512x384 (32,64)/512x384 (64,32)/512x384"
+
+test_rpf_cropping() {
+	test_start "RPF crop from $crop"
+
+	pipe_configure rpf-wpf 0 0
+	format_configure rpf-wpf 0 0 RGB24 1024x768 ARGB32 --rpfcrop=$crop
+
+	vsp_runner rpf.0 &
+	vsp_runner wpf.0
+
+	local result=$(compare_frames crop=${crop})
+
+	test_complete $result
+}
+
+test_main() {
+	local crop
+
+	for crop in $crops ; do
+		test_rpf_cropping $crop
+	done
+}
+
+test_init $0 "$features"
+test_run
