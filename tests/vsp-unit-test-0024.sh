f347373bfe8857b880f10c8a04a3f5fdd88d0fde
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2017-05-25 17:04:49 +0300

    tests: Add BRS test
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Reviewed-by: Kieran Bingham <kieran.bingham+renesas@ideasonboard.com>
    Tested-by: Kieran Bingham <kieran.bingham+renesas@ideasonboard.com>

---
+++ tests/vsp-unit-test-0024.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+#
+# Test composition through the BRS in RGB and YUV formats.
+#
+
+. ./vsp-lib.sh
+
+features="rpf.0 rpf.1 brs wpf.0"
+formats="RGB24 YUV444M"
+
+test_brs() {
+	local format=$1
+	local ninputs=$2
+
+	test_start "BRS in $format with $ninputs inputs"
+
+	pipe_configure rpf-brs $ninputs
+	format_configure rpf-brs $format 1024x768 $ninputs
+
+	local input
+	for input in `seq 0 1 $((ninputs-1))` ; do
+		vsp_runner rpf.$input &
+	done
+	vsp_runner wpf.0
+
+	local result=$(compare_frames)
+
+	test_complete $result
+}
+
+test_main() {
+	local num_inputs=2
+	local format
+	local ninputs
+
+	for format in $formats ; do
+		for ninputs in `seq $num_inputs` ; do
+			test_brs $format $ninputs
+		done
+	done
+}
+
+test_init $0 "$features"
+test_run
