2d3ff1058aa745020a2397873cf95c8bfccfc02e
Kieran Bingham <kieran.bingham@ideasonboard.com>
2020-09-16 14:32:15 +0100

    scripts/logger: Use new monotonic-ts tool
    Utilise the new monotonic timestamping tool to remove the manual parsing of
    timestamps via /proc/timer_list which can only be read by root.
    
    This also simplifies the processing required and contains all timestamping
    actions within a single process space.
    
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>
    Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    
    ---
    v2:
     - Fix shellcheck issues

--- scripts/logger.sh
+++ scripts/logger.sh
@@ -2,23 +2,11 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # SPDX-FileCopyrightText: 2016 Renesas Electronics Corporation
 
-now() {
-	awk '/^now/ {time=$3; printf("[%u.%06u]", time / 1000000000, (time % 1000000000) / 1000) ; exit}' /proc/timer_list
-}
-
 label=${1:+ [$1]}
 
 TRACE_MARKER=/sys/kernel/debug/tracing/trace_marker
-if [ -e $TRACE_MARKER ]; then
-	extra_log_files=$TRACE_MARKER
+if [ -e $TRACE_MARKER ] && [ "$(id -u)" = 0 ]; then
+	./monotonic-ts "$label" | tee -a $TRACE_MARKER
+else
+	./monotonic-ts "$label"
 fi
-
-while read line ; do
-	newline="$(now)$label $line"
-
-	echo "$newline"
-
-	for f in $extra_log_files; do
-		echo "$newline" >> $f;
-	done;
-done
